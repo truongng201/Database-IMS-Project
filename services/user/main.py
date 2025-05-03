fe249659f23ffbdd5e6c2e762c34f0920ccacad0
Nemo <truongng201@gmail.com>
2025-05-04 02:01:23 +0700

    big update

--- services/user/main.py
+++ services/user/main.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI
 from routes import router
 
 ENV = os.getenv("ENV", "development")
-SERVICE_NAME = os.getenv("SERVICE_NAME", None)
-APP_VERSION = os.getenv("APP_VERSION", None) if ENV == "production" else ENV
+SERVICE_NAME = os.getenv("SERVICE_NAME", "service")
+APP_VERSION = os.getenv("APP_VERSION", "version") if ENV == "production" else ENV
 API_VERSION = os.getenv("API_VERSION", "v1")
 
 app = FastAPI(title=SERVICE_NAME, version=APP_VERSION, root_path=f"/{API_VERSION}/{SERVICE_NAME}")
