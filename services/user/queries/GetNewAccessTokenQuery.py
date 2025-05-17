a7f4582cd222a6719db53eb57a28206864295e0e
Nemo <truongng201@gmail.com>
2025-05-17 14:27:21 +0700

    minor update

--- services/user/queries/GetNewAccessTokenQuery.py
+++ services/user/queries/GetNewAccessTokenQuery.py
@@ -6,7 +6,13 @@ class GetNewAccessTokenQuery:
         
     def execute(self, refresh_token: str):
         query = """
-        SELECT user_id FROM login_logs
+        SELECT 
+            users.user_id as user_id,
+            users.email as email,
+            users.role_name as role_name,
+            users.warehouse_id as warehouse_id
+        FROM login_logs
+        LEFT JOIN users ON login_logs.user_id = users.user_id
         WHERE refresh_token = %s
         """
         params = (refresh_token,)
@@ -16,8 +22,11 @@ class GetNewAccessTokenQuery:
             return False
         
         return {
-            "user_id": res[0][0]
+            "user_id": res[0][0],
+            "email": res[0][1],
+            "role_name": res[0][2],
+            "warehouse_id": res[0][3]
         }
-        
+
     def close(self):
         self.db.close_pool()
